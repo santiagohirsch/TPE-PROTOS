6f91f887e6cf27308dab96be37732fef8e9a7452
santiagohirsch <shirsch@itba.edu.ar>
2023-11-20 11:27:48 -0300

    refactor: added wrapper functions

--- src/server/server_utils.c
+++ src/server/server_utils.c
@@ -9,7 +9,61 @@
 
 
 
-//TODO: Cambiar los checkeos de errores a otras funciones :)
+//TODO: Agregar codigos de error al .h
+
+int w_socket(int domain, int type, int protocol){
+    int ret = socket(domain, type, protocol);
+    if(ret < 0){
+        perror("error creating socket");
+        exit(1);
+    }
+    return ret;
+}
+
+int w_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen){
+    int ret = bind(sockfd, addr, addrlen);
+    if(ret < 0){
+        perror("error binding socket");
+        exit(1);
+    }
+    return ret;
+}
+
+int w_listen(int sockfd, int backlog){
+    int ret = listen(sockfd, backlog);
+    if(ret < 0){
+        perror("error listening socket");
+        exit(1);
+    }
+    return ret;
+}
+
+int w_accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen){
+    int ret = accept(sockfd, addr, addrlen);
+    if(ret < 0){
+        perror("error accepting socket");
+        exit(1);
+    }
+    return ret;
+}
+
+int w_recv(int sockfd, void *buf, size_t len, int flags){
+    int ret = recv(sockfd, buf, len, flags);
+    if(ret < 0){
+        perror("error receiving socket");
+        exit(1);
+    }
+    return ret;
+}
+
+int w_send(int sockfd, const void *buf, size_t len, int flags){
+    int ret = send(sockfd, buf, len, flags);
+    if(ret < 0){
+        perror("error sending socket");
+        exit(1);
+    }
+    return ret;
+}
 
 int setup_server(int port) {
 
@@ -19,30 +73,15 @@ int setup_server(int port) {
     sock_address.sin_addr.s_addr = htonl(INADDR_ANY);
     sock_address.sin_port = htons(port);
     
-    int server = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
-    if(server < 0){
-        perror("error creating socket");
-        goto finish;
-    }
+    int server = w_socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 
     setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &(int){ 1 }, sizeof(int));
 
-    if(bind(server,(struct sockaddr *)&sock_address, sizeof(sock_address)) < 0){
-        perror("bind error");
-        goto finish;
-    }
+    w_bind(server, (struct sockaddr *) &sock_address, sizeof(sock_address));
 
-    if (listen(server, 20) < 0) {
-        perror("listen error");
-        goto finish;
-    }
+    w_listen(server, 50);
 
-    int ret = 0;
-    return ret;
-
-    finish:
-    ret = -1;
-    return ret;
+    return 0;
 }
 
 int accept_connection(int server_sock){
@@ -50,32 +89,18 @@ int accept_connection(int server_sock){
     struct sockaddr_storage client_address;
     socklen_t  client_address_len = sizeof(client_address);
 
-    int client_socket = accept(server_sock,(struct sockaddr *) &client_address, &client_address_len);
-    if(client_socket < 0){
-        perror("accept error");
-        return -1;
-    }
+    int client = w_accept(server_sock, (struct sockaddr *) &client_address, &client_address_len);
 
-    return client_socket;
+    return client;
 }
 
 int handle_connection(int client) {
     char buffer[BUFF_SIZE] = {0};
     int bytes_read = 0;
 
-    while((bytes_read = recv(client, buffer, BUFF_SIZE, 0)) > 0){
+    while((bytes_read = w_recv(client, buffer, BUFF_SIZE, 0)) > 0){
         
-        if (bytes_read < 0){
-            perror("recv error");
-            return -1;
-        }
-
-        int bytes_sent = send(client, buffer, bytes_read, 0);
-
-        if(bytes_sent < 0){
-            perror("send error");
-            return -1;
-        }
+        int bytes_sent = w_send(client, buffer, bytes_read, 0);
 
         if(bytes_sent != bytes_read){
             perror("send error");
--- src/server/server_utils.h
+++ src/server/server_utils.h
@@ -26,4 +26,53 @@ int accept_connection(int server);
 *   - params: client socket file descriptor
 */
 int handle_connection(int client);
+
+/* W_SOCKET
+*   - creates a socket
+*   - returns the socket file descriptor
+*   - exits on error
+*   - params: domain, type, protocol
+*/
+int w_socket(int domain, int type, int protocol);
+
+/* W_BIND
+*   - binds a socket to an address
+*   - returns 0 on success
+*   - exits on error
+*   - params: socket file descriptor, address, address length
+*/
+int w_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+
+/* W_LISTEN
+*   - listens on a socket
+*   - returns 0 on success
+*   - exits on error
+*   - params: socket file descriptor, backlog
+*/
+int w_listen(int sockfd, int backlog);
+
+/* W_ACCEPT
+*   - accepts a connection on a socket
+*   - returns the socket file descriptor
+*   - exits on error
+*   - params: socket file descriptor, address, address length
+*/
+int w_accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+
+/* W_RECV
+*   - receives data on a socket
+*   - returns the number of bytes received
+*   - exits on error
+*   - params: socket file descriptor, buffer, buffer length, flags
+*/
+int w_recv(int sockfd, void *buf, size_t len, int flags);
+
+/* W_SEND
+*   - sends data on a socket
+*   - returns the number of bytes sent
+*   - exits on error
+*   - params: socket file descriptor, buffer, buffer length, flags
+*/
+int w_send(int sockfd, const void *buf, size_t len, int flags);
+
 #endif
